e0faa3a62f79b6ea185c56afcea147794eaec52c
Semih <semih.guner.2002@gmail.com>
2022-07-08 09:52:17 +0300

    fotoBasar php eklendi

---
+++ fotoBasar.php
@@ -0,0 +1,30 @@
+<?php
+if(!empty($_GET['id'])){
+
+    $host     = 'mariadb';
+    $username = 'semih';
+    $password = 'semih';
+    $db     = 'galeridb';
+    
+    //Create the connection and select the database
+    $db = new mysqli($host, $username, $password, $db);
+    
+    // Check the connection
+    if($db->connect_error){
+        die("Connexion error: " . $db->connect_error);
+    }
+    
+    //Get the image from the database
+    $res = $db->query("SELECT img FROM fotolar WHERE id = {$_GET['id']}");
+    
+    if($res->num_rows > 0){
+        $img = $res->fetch_assoc();
+        
+        //Render the image
+        header("Content-type: image/png"); 
+        echo $img['image']; 
+    }else{
+        echo 'Image not found...';
+    }
+}
+?>
--- galeri.php
+++ galeri.php
@@ -22,38 +22,6 @@
     </style>
   </head>
   <body>
-    <!-- status check starts -->
-    <aside>
-      <div class="container-fluid pb-0 mb-0">
-        <div class="row">
-          <div class="col mr-0 ml-0 mt-5 pt-0">
-            <?php
-              $servername = "mariadb";
-              $username = "semih";
-              $password = "semih";
-              $database = "galeridb";
-              $mysqli = new mysqli($servername,$username,$password,$database,3306);
-              if ($mysqli -> connect_errno) 
-              { 
-                echo "<div class=\"mt-2 float-right\">";
-                echo "<img src=\"./fotolar/kizilampul.png\" alt=\"kırmızı yanan ampül png'si\" width=\"30px\">";
-                echo "<h2 style=\"display:inline; color:red; font-size:20px;\">Hata! MySQL bağlantısı yapılamadı: ($mysqli -> connect_error)</h2>";
-                echo "</div>";
-                exit();
-              } 
-              else
-              {
-                echo "<div class=\"mt-2 float-right\">";
-                echo "<img src=\"./fotolar/ampul.png\" alt=\"yanan ampül png'si\" width=\"30px\">";
-                echo "<h2 style=\"display:inline; color:green; font-size:20px;\">MariaDB bağlandı.</h2>";
-                echo "</div>";
-              }
-            ?>
-          </div>
-        </div>
-      </div>
-    </aside> 
-    <!-- status check ends -->
     <!-- header starts-->
     <header>
       <div class="container">
@@ -93,13 +61,13 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="https://picsum.photos/1920/600" class="d-block w-100" alt="IMG 1">
+          <img src="fotoBasar.php?id=1" class="d-block w-100" alt="IMG 1">
         </div>
         <div class="carousel-item">
-          <img src="https://picsum.photos/1920/600" class="d-block w-100" alt="IMG 2">
+          <img src="fotoBasar.php?id=2" class="d-block w-100" alt="IMG 2">
         </div>
         <div class="carousel-item">
-          <img src="https://picsum.photos/1920/600" class="d-block w-100" alt="IMG 3">
+          <img src="fotoBasar.php?id=3" class="d-block w-100" alt="IMG 3">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-target="#GaleriCarouseli" data-slide="prev">
